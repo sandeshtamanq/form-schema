d2a24843827d2388ee7d387d019b1d5463d3465c
codeinvisible <lamasandesh400@gmail.com>
2022-11-07 11:45:52 +0545

    type validation

--- src/components/validation/error-handler.js
+++ src/components/validation/error-handler.js
@@ -1,8 +1,9 @@
 const INVALID_INPUT = "Invalid Input";
 
 const errorHandler = (value, validate) => {
-  if (value.length < validate.min_length) return INVALID_INPUT;
-  if (value < validate.min_age) return INVALID_INPUT;
+  if (value.length < validate.min_length && typeof value === validate.type)
+    return INVALID_INPUT;
+  if (value < validate.min_age && !isNaN(value)) return INVALID_INPUT;
 };
 
 export default errorHandler;
