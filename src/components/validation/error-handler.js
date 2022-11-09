f81e6e74b8066291c691d04198216cae4614e844
codeinvisible <lamasandesh400@gmail.com>
2022-11-09 12:04:51 +0545

    validation error fixed

--- src/components/validation/error-handler.js
+++ src/components/validation/error-handler.js
@@ -3,11 +3,15 @@ const errorHandler = (value, validate) => {
     case "string":
       if (value.length < validate.min_length || !value.match(/^[a-zA-Z ]*$/)) {
         return true;
+      } else {
+        return false;
       }
 
     case "number":
       if (value < validate.min_age || !value.match(/^[0-9]*$/)) {
         return true;
+      } else {
+        return false;
       }
   }
 };
